f2291de9669034107be99b26fa1f80cf754958c4
Raisa Litchfield <raisa.litch@gmail.com>
2017-10-14 17:45:14 +1100

    added plane and player

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	public float moveSpeed = 100;
+
+
+	// Update is called once per frame
+	void Update () {
+
+		// KEY MOVEMENTS (forwards, backwards, right, left, roll)
+
+		// Move camera forwards
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			this.transform.localPosition += this.transform.forward * Time.deltaTime * moveSpeed;
+
+		// Move camera backwards
+		} else if (Input.GetKey(KeyCode.DownArrow)) {
+			this.transform.localPosition -= this.transform.forward * Time.deltaTime * moveSpeed;
+
+		// Move camera right
+		} else if (Input.GetKey(KeyCode.RightArrow)) {
+			this.transform.localPosition += this.transform.right * Time.deltaTime * moveSpeed;
+
+		// Move camera left
+		} else if (Input.GetKey(KeyCode.LeftArrow)) {
+			this.transform.localPosition -= this.transform.right * Time.deltaTime * moveSpeed;
+		}
+
+	}
+
+}
---
+++ Assets/SunMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunMove : MonoBehaviour
+{
+
+	public float radius = 1000;		// the radius of the circle that the sun rotates around (sun follows the circumference)
+    public float speed = 10;		// speed at which the sun moves
+
+    // Update is called once per frame
+    void Start()
+    {
+		
+        this.transform.localScale = new Vector3(100, 100, 100);
+
+    }
+
+
+    void Update()
+    {
+		// Moves the sun in a circlular motion so it rises and sets
+        this.transform.position = new Vector3(Mathf.Cos((Time.fixedTime/5)*speed), Mathf.Sin((Time.fixedTime/5)*speed), 0.0f) * radius;
+
+    }
+}
